447f3ed33b57f88e83bd6cdbbadea99a6f1105f3
Eduardo-Boanca <s7813818b@studenti.itisavogadro.it>
2021-08-31 14:32:57 +0200

    Modifiche mastermind

--- Esercizi Informatica Estate/Master Mind/masterMind.cpp
+++ Esercizi Informatica Estate/Master Mind/masterMind.cpp
@@ -1,10 +1,19 @@
 /* 
     Eduardo Boanca 3^C Informatica
-    
+    Questo programma ti permette di giocare al gioco del Master Mind.
+    In 10 tentativi bisogna indovinare un numero di 4 cifre pensato
+    dal computer. Saranno 4 cifre singole contenute in un vettore Int
+    di 4 posizioni. Ad ogni tiro il computer dovrà dire quante cifre sono
+    state indovinate e in che posizione del vettore
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char const *argv[])
 {
     /* code */
+    printf("CIAO A TUTTI");
     return 0;
 }
