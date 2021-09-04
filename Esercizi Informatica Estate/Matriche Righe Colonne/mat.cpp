052dbae91a545133197f365a5cde6a589496f31a
Eduardo-Boanca <s7813818b@studenti.itisavogadro.it>
2021-09-04 12:03:22 +0200

    Codice per esercizio sulla matrice

--- Esercizi Informatica Estate/Matriche Righe Colonne/mat.cpp
+++ Esercizi Informatica Estate/Matriche Righe Colonne/mat.cpp
@@ -5,6 +5,44 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#define COL 10
+#define RIG 5
+
+void creaMatrice(int mat[][COL])
+{
+    srand(time(0));
+
+    for(int i = 0; i < RIG; i++)
+        for(int j = 0; j < COL; j++)
+            mat[i][j] = rand() % 100 + 1;
+}
+
+void maxRiga(int mat[][COL])
+{
+    int max = -1;
+
+    for(int i = 0; i < RIG; i++)
+    {
+        for(int j = 0; j < COL; j++)
+            if(mat[i][j] > max)
+                max = mat[i][j];
+        printf("numero minimo della riga %d e': %d\n", i, max);
+        max = -1;
+    }
+}
+
+int minMatrice(int mat[][COL])
+{
+    int min = mat[0][0];
+    for(int i = 0; i < RIG; i++)
+    {
+        for(int j = 0; j < COL; j++)
+            if(mat[i][j] < min)
+                min = mat[i][j];
+    }
+    return min;
+}
 
 int main(int argc, char const *argv[])
 {
