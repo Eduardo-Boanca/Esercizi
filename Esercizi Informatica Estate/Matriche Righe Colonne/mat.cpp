d5129a3cc1267aa22345133770aced6c3122eb95
Eduardo-Boanca <s7813818b@studenti.itisavogadro.it>
2021-09-04 12:53:58 +0200

    fine es matrice

--- Esercizi Informatica Estate/Matriche Righe Colonne/mat.cpp
+++ Esercizi Informatica Estate/Matriche Righe Colonne/mat.cpp
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define COL 10
+#define COL 5
 #define RIG 5
 
 void creaMatrice(int mat[][COL])
@@ -27,7 +27,7 @@ void maxRiga(int mat[][COL])
         for(int j = 0; j < COL; j++)
             if(mat[i][j] > max)
                 max = mat[i][j];
-        printf("numero minimo della riga %d e': %d\n", i, max);
+        printf("numero massimo della riga n.%d e': %d\n", i, max);
         max = -1;
     }
 }
@@ -44,8 +44,42 @@ int minMatrice(int mat[][COL])
     return min;
 }
 
+void mediaCol(int mat[][COL])
+{
+    float somma = 0;
+    float media = 0;
+    
+    for(int j = 0; j < COL; j++)
+    {
+        for(int i = 0; i < RIG; i++)
+            somma += mat[i][j];
+        somma = somma/RIG;
+        printf("la media della colonna %d e': %.2f\n",j, somma);
+        somma = 0;
+    }
+}
+
+void stampaMatrice(int mat[][COL])
+{
+    for(int i = 0; i < RIG; i++)
+    {
+        printf("\n");
+        for(int j = 0; j < COL; j++)
+            printf(" %3d ", mat[i][j]);
+    }
+    printf("\n\n");
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int mat[RIG][COL];
+
+    creaMatrice(mat);
+    stampaMatrice(mat);
+    maxRiga(mat);
+    printf("\nIl numero minimo della matrice e': %d\n", minMatrice(mat));
+    printf("\n");
+    mediaCol(mat);
+
     return 0;
 }
