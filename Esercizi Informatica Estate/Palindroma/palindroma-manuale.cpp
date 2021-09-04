7ef7bae4b50037a2be64f7c4164bc5f300ba1240
Eduardo-Boanca <s7813818b@studenti.itisavogadro.it>
2021-09-04 11:31:18 +0200

    Aggiunta nuovo esercizio sul Palindromo

--- Esercizi Informatica Estate/Palindroma/palindroma-manuale.cpp
+++ Esercizi Informatica Estate/Palindroma/palindroma-manuale.cpp
@@ -9,16 +9,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <iostream>
 #define LEN 20
 
 void input(char parola[])
 {
-    
+    printf("Inserisci la parola: ");
+    gets(parola);
+}
+
+void toupperParola(char parola[])
+{
+    for (int i = 0; i < strlen(parola); i++)
+        parola[i] = toupper(parola[i]);
+}
+
+bool checkPalind(char parola[])
+{
+    toupperParola(parola);
+    bool checkPalind = true;
+    for (int i = 0; i < strlen(parola)/2 && checkPalind; i++)
+        if(parola[i] != parola[(strlen(parola)-1)-i])
+            checkPalind = false;
+
+    return checkPalind;
 }
 
 int main(int argc, char const *argv[])
 {
     char parola[LEN];
+
+    input(parola);
+    if(checkPalind(parola))
+        printf("La stringa inserita (%s) è palindroma", parola);
+    else
+        printf("La stringa inserita (%s) non è palindroma", parola);
 
     return 0;
 }
