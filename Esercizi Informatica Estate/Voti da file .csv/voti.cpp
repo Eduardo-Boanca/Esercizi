0b006443f0b141b0234bc72455e701788b085f8c
Eduardo-Boanca <s7813818b@studenti.itisavogadro.it>
2021-09-07 18:44:58 +0200

    aggiunta commenti

--- Esercizi Informatica Estate/Voti da file .csv/voti.cpp
+++ Esercizi Informatica Estate/Voti da file .csv/voti.cpp
@@ -15,7 +15,6 @@
 #define LEN_NOME 50
 #define NUM_STUDENTI 40
 #define ERRORE -1
-#define OK 1
 
 typedef struct 
 {
@@ -23,6 +22,12 @@ typedef struct
     float voto;
 } studente;
 
+/*----------------------------------------------------------------
+Legge da un file csv i nomi e i voti e li inserisce in un array
+*   Parametri:
+*        studente voti[]: output: dove verrano inseriti i dati letti
+*        return i: return del numeri di dati letti
+----------------------------------------------------------------*/
 int caricaVoti(studente voti[])
 {
     FILE * f;
@@ -37,21 +42,37 @@ int caricaVoti(studente voti[])
         return ERRORE;
     
     for(i = 0; !feof(f); i++)
-        fscanf(f,"\n%[^,], %f", voti[i].nome, &voti[i].voto);
+        fscanf(f,"\n%[^,], %f", voti[i].nome, &voti[i].voto); //%[^,] prende il contenuto fino alla virgola
 
-    return i-1;
+    return i;
 }
 
+/*----------------------------------------------------------------
+Esegue la media di tutti i voti nel file
+*    Parametri:
+*         studente voti[]: input: dati dell'alunno
+*         int len: input: fino a dove vengono considerati i dati
+*         return media: return del valore della media di tutti i voti
+----------------------------------------------------------------*/
 float mediaVoti(studente voti[], int len)
 {
     float somma = 0;
+    float media = 0;
     for(int i = 0; i < len; i++)
         if(voti[i].voto != 0)
             somma += voti[i].voto;
 
-    return (float) somma/len;
+    media = somma/len;
+    return media;
 }
 
+/*----------------------------------------------------------------
+Controlla quale alunno ha il voto piu alto
+*    Parametri:
+*         studente voti[]: dati alunni
+*        int len: fino a dove vengono considerati i dati
+*         return maggiore: return del voto piu alto
+----------------------------------------------------------------*/
 studente votoAlto(studente voti[], int len)
 {
     studente maggiore;
@@ -63,6 +84,12 @@ studente votoAlto(studente voti[], int len)
     return maggiore;
 }
 
+/*----------------------------------------------------------------
+Trova tutti gli alunni con un voto insufficiente
+*   Parametri:
+*      studente voti[]: input: dati alunni
+*        int len: fino a dove vengono considerati i dati
+----------------------------------------------------------------*/
 void votiInsuff(studente voti[], int len)
 {
     for(int i = 0; i < len; i++)
@@ -70,22 +97,38 @@ void votiInsuff(studente voti[], int len)
             printf("Lo studente %s ha ricevuto un voto insufficiente che equivale a: %0.2f\n", voti[i].nome,voti[i].voto);
 }
 
+/*----------------------------------------------------------------
+Calcola la percentuale degli assenti tra tutti gli alunni
+*    Parametri:
+*     studente voti[]: input: dati alunni
+*        int len: fino a dove vengono considerati i dati
+*         return percentAssenti: return della perecentuale assenti
+----------------------------------------------------------------*/
 float percentualeAssenti(studente voti[], int len)
 {
     int numAssenti = 0;
+    float percentAssenti = 0;
     for(int i = 0; i < len; i++)
         if(voti[i].voto == 0)
             numAssenti++;
+    percentAssenti = (numAssenti * 100)/len;
     
-    return (float) (numAssenti*100)/len;
+    return percentAssenti;
 }
 
+/*----------------------------------------------------------------
+Stampa i nomi degli alunni con i loro rispettivi voti
+*    Parametri:
+*     studente voti[]: input: dati alunni
+*        int len: fino a dove vengono considerati i dati
+----------------------------------------------------------------*/
 void stampaVoti(studente voti[], int len)
 {
     for (int i = 0; i < len; i++)
-        printf("Alunno: %s: Voto: %0.2f\n", voti[i].nome,voti[i].voto);
+        printf("Alunno: %s:  |  Voto: %0.2f\n", voti[i].nome,voti[i].voto);
 }
 
+/*======MAIN======*/
 int main(int argc, char const *argv[])
 {
     studente voti[NUM_STUDENTI];
@@ -94,21 +137,19 @@ int main(int argc, char const *argv[])
 
     if(numStudent == ERRORE)
         return EXIT_FAILURE;
+
+    stampaVoti(voti, numStudent);
     
-    printf("La media dei voti e': %.3f", mediaVoti(voti, numStudent));
+    printf("\nLa media dei voti e': %.3f\n", mediaVoti(voti, numStudent));
     printf("\n");
     
     votoPiuAlto = votoAlto(voti, numStudent);
-    printf("L'alunno con il voto piu alto e': %s con il voto di %.2f", votoPiuAlto.nome, votoPiuAlto.voto);
-    printf("\n");
+    printf("L'alunno/a con il voto piu alto e': %s con il voto di %.2f\n", votoPiuAlto.nome, votoPiuAlto.voto);
 
     votiInsuff(voti, numStudent);
     printf("\n");
 
-    printf("La percentuale di studenti assenti e': %.2f%%", percentualeAssenti(voti, numStudent));
-    printf("\n");
-
-    stampaVoti(voti, numStudent);
+    printf("La percentuale di studenti assenti e': %.2f%%\n", percentualeAssenti(voti, numStudent));
 
         return EXIT_SUCCESS;
 }
